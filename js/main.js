793ca3bde5fb2ace2609408b096447eb4e7a0b7e
Jose Carmen Hernandez <jcarmenh156@gmail.com>
2024-04-14 13:23:29 -0600

    stock

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ const getData = async (url) => {
     crearHtml(tienda);
     //llamar a agregarEventos:
 
-    agregarEventos(tienda);
+    agregarEventos();
   } catch (error) {
     console.error("Error al obtener los datos:", error);
   }
@@ -55,13 +55,13 @@ function filtrarProducto(arr, filtro) {
 }
 //FUNCION AGREGAR EVENTOS:
 //Evento click en los botones "Agregar" y "quitar" de las tarjetas de producto:
+let cantidadInput;
 function agregarEventos() {
   const agregarBtns = document.querySelectorAll(".agregar-btn");
   const quitarBtns = document.querySelectorAll(".quitar-btn");
 
   agregarBtns.forEach((btn) => {
     btn.addEventListener("click", agregarAlCarrito);
-    /* TOASTIFY*/
     btn.addEventListener("click", (event) => {
       const productoId = parseInt(event.target.getAttribute("data-id"));
       const productoSeleccionado = tienda.find(
@@ -86,6 +86,18 @@ function agregarEventos() {
   quitarBtns.forEach((btn) => {
     btn.addEventListener("click", (event) => quitarDelCarrito(event, tienda));
   });
+
+  //Inicializa la variable cantidadInput con el primer elemento del nodo cantidadInputs:
+
+  const cantidadInputs = document.querySelectorAll(".cantidad");
+  cantidadInput = cantidadInputs[0];
+
+  //Agrega eventos a los input con la class="cantidad":
+  cantidadInputs.forEach((input) => {
+    input.addEventListener("change", (event) => {
+      cantidadInput = event.target;
+    });
+  });
 }
 
 //Funcion para agregar un producto al carrito:
@@ -93,38 +105,58 @@ function agregarAlCarrito(event) {
   console.log("Agregando al carrito");
   const productoId = parseInt(event.target.getAttribute("data-id"));
 
+  //cantidad de producto:
+  const cantidadSeleccionada = cantidadInput
+    ? parseInt(cantidadInput.value)
+    : 1; //Establece el valor 1 por defecto si cantidadInput es null o undefined.
+
   const productoSeleccionado = tienda.find(
     (producto) => producto.id === productoId
   );
 
   if (productoSeleccionado) {
-    //verificar si el producto ya esta en el carrito:
-    const existeEnCarrito = carrito.some((item) => item.id === productoId);
+    //Verificar si la cantidad seleccionada no supera el stock disponible:
+    if (cantidadSeleccionada <= productoSeleccionado.stock) {
+      //verificar si el producto ya esta en el carrito:
+      const existeEnCarrito = carrito.some((item) => item.id === productoId);
 
-    if (existeEnCarrito) {
-      //si ya esta en el carrito incrementa la cantidad:
-      carrito.forEach((item) => {
-        if (item.id === productoId) {
-          item.cantidad++;
-        }
-      });
+      if (existeEnCarrito) {
+        //si ya esta en el carrito incrementa la cantidad:
+        carrito.forEach((item) => {
+          if (item.id === productoId) {
+            //Asignar la cantidad seleccionada:
+            item.cantidad = cantidadSeleccionada;
+          }
+        });
+      } else {
+        //si no esta en el carrito agregarlo con la cantidad seleccionada:
+        carrito.push({
+          ...productoSeleccionado,
+          cantidad: cantidadSeleccionada,
+        });
+      }
+      //Despues de agregar un producto al carrito, actualizar la variable totalPagar en el almacenamiento local:
+      const totalPagar = calcularPagoTotal(carrito);
+      localStorage.setItem("totalPagar", JSON.stringify(totalPagar));
+      //Actualizar contenido del contenedor de pago con el nuevo total:
+      contenedorPago.innerHTML = `<p>Total del carrito: $${totalPagar.toFixed(
+        2
+      )}</p>`;
+      //guardar carrito actualizado en LS:
+      localStorage.setItem("carrito", JSON.stringify(carrito));
     } else {
-      //si no esta en el carrito agregarlo con cantidad 1:
-      carrito.push({ ...productoSeleccionado, cantidad: 1 });
+      //Mostrar un mensaje si la cantidad seleccionada supera el stock disponible:
+      Swal.fire({
+        icon: "error",
+        title: "Lo sentimos",
+        text: "Stock insuficiente",
+      });
     }
-
-    //Despues de agregar un producto al carrito, actualizar la variable totalPagar en el almacenamiento local:
-    const totalPagar = calcularPagoTotal(carrito);
-    localStorage.setItem("totalPagar", JSON.stringify(totalPagar));
-    //Actualizar contenido del contenedor de pago con el nuevo total:
-    contenedorPago.innerHTML = `<p>Total del carrito: $${totalPagar}.00</p>`;
-    //guardar carrito actualizado en LS:
-    localStorage.setItem("carrito", JSON.stringify(carrito));
   }
 }
 
 //FUNCION PARA QUITAR UN PRODUCTO DEL CARRITO:
-function quitarDelCarrito(event) {
+function quitarDelCarrito(event, tienda) {
   const productoId = parseInt(event.target.getAttribute("data-id"));
   const indice = carrito.findIndex((item) => item.id === productoId);
 
@@ -139,14 +171,22 @@ function quitarDelCarrito(event) {
     carrito.splice(indice, 1); // Elimina el producto del carrito
     localStorage.setItem("carrito", JSON.stringify(carrito)); // Actualiza el carrito en el localStorage
     crearHtml(carrito); // Vuelve a renderizar el carrito
-    contenedorPago.innerHTML = `<p>Total del carrito: $${totalPagar}.00</p>`;
+    contenedorPago.innerHTML = `<p>Total del carrito: $${totalPagar.toFixed(
+      2
+    )}</p>`;
 
     localStorage.setItem("totalPagar", JSON.stringify(totalPagar)); //actualiza el total a pagar en el LS
     console.log(totalPagar);
+
+    //Si el carrito se queda vacio mostrar la alerta:
+    if (carrito.length === 0) {
+      Swal.fire("Carrito vacio");
+      crearHtml(tienda);
+    }
   }
 }
 
-/*-----PRUEBA DE FUNCION PARA AJUSTAR BOTONES----- */
+/*FUNCION CREAR HTML*/
 function crearHtml(arr) {
   contenedor.innerHTML = "";
 
@@ -154,7 +194,7 @@ function crearHtml(arr) {
   if (arr && arr.length > 0) {
     // Desestructuración del objeto en la función:
     for (const el of arr) {
-      const { img, nombre, precio, id } = el;
+      const { img, nombre, precio, id, stock } = el;
       const estaEnCarrito = carrito.some((item) => item.id === id);
       //El precio del producto tendra 2 decimales:
       const html = `
@@ -163,6 +203,15 @@ function crearHtml(arr) {
           <hr>
           <h3>${nombre}</h3>
           <p>Precio: $${precio.toFixed(2)} </p>
+          ${
+            !estaEnCarrito
+              ? `<p>Disponibles: ${stock}</p>
+            <div class="counter">
+            <input type="number" class="cantidad" id="ingreso" min="1" max="${stock}" value="1" />
+            
+          </div>`
+              : ""
+          }
           <div class="card-action">
             ${
               estaEnCarrito
@@ -176,15 +225,10 @@ function crearHtml(arr) {
       contenedor.innerHTML += html;
     }
     agregarEventos(); // Agrega eventos después de crear todas las tarjetas HTML.
-  } else {
-    // Si no hay productos agregados al carrito, la función muestra el siguiente mensaje:
-    contenedorPago.innerHTML = "";
-    contenedor.innerHTML =
-      "No hay productos agregados al carrito.  Haz click en 'Buscar' para ver todos nuestros productos";
   }
 }
 
-//crearHtml(tienda);
+crearHtml(tienda);
 
 //Agregar una escucha de evento click al boton "buscar":
 btnSearch.addEventListener("click", () => {
@@ -205,16 +249,27 @@ btnSearch.addEventListener("click", () => {
 //Los productos agregados al carrito solo tienen el boton "quitar" y no se pueden agregar de nuevo dentro del carrito:
 btnMostrar.addEventListener("click", () => {
   const carritoDesdeLS = JSON.parse(localStorage.getItem("carrito"));
+  //Verificar si el carrito esta vacio:
+  if (!carritoDesdeLS || carritoDesdeLS.length === 0) {
+    //No hacer nada si el carrito esta vacio:
+    return;
+  }
   crearHtml(carritoDesdeLS, false); //false
 
   //Calcular pago:
   const total = calcularPagoTotal(carritoDesdeLS);
-  contenedorPago.innerHTML = `<p>Total del carrito: $${total}.00</p>`;
+  contenedorPago.innerHTML = `<p>Total del carrito: $${total.toFixed(2)}</p>`;
   console.log(total);
 });
 
 //Agregar una escucha de evento click al boton "vaciar carrito":
 btnVaciarCarrito.addEventListener("click", () => {
+  const carritoDesdeLS = JSON.parse(localStorage.getItem("carrito"));
+  //Verificar si el carrito esta vacio:
+  if (!carritoDesdeLS || carritoDesdeLS.length === 0) {
+    //no hacer nada si el carrito esta vacio:
+    return;
+  }
   //Restablecer la variable totalPagar a cero en el almacenamiento local:
   localStorage.setItem("totalPagar", JSON.stringify(0));
 
@@ -252,6 +307,20 @@ btnPagarCarrito.addEventListener("click", () => {
     //no hacer nada si el carrito esta vacio:
     return;
   }
+
+  //Iterar sobre cada elemento del carrito:
+  carritoDesdeLS.forEach((productoEnCarrito) => {
+    //Encontrar el producto correspondiente en la tienda:
+    const productoEnTienda = tienda.find(
+      (producto) => producto.id === productoEnCarrito.id
+    );
+
+    if (productoEnTienda) {
+      //Restar la cantidad comprada al stock:
+      productoEnTienda.stock -= productoEnCarrito.cantidad;
+    }
+  });
+
   const total = calcularPagoTotal(carritoDesdeLS);
   //Limpiar el contenedor del total a pagar:
   contenedorPago.innerHTML = "";
@@ -262,10 +331,12 @@ btnPagarCarrito.addEventListener("click", () => {
   //Despues de pagar, reiniciar la variable totalPagar a cero en el almacenamiento local:
   localStorage.setItem("totalPagar", JSON.stringify(0));
 
+  //Actualizar las tarjetas de producto en la tienda para mostrar stock actualizado:
+  crearHtml(tienda);
+
   Swal.fire({
     title: "Compra exitosa",
-    text: `Total pagado: $${total}`,
+    text: `Total pagado: $${total.toFixed(2)}`,
     icon: "success",
   });
-  crearHtml(tienda);
 });
